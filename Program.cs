3989fb122dbdbe549fb39e2df0b1febb5df6f766
emberogl <embernebulan@gmail.com>
2023-09-11 10:19:38 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Superheroes
+{
+    internal class Program
+    {
+        // Creating a super hero, setting information, and outputting attribute data.
+        static void Main()
+        {
+            SuperHero superPerson = new SuperHero();
+
+            superPerson.SetIdentity("John Doe");
+
+            superPerson.alias = "Super Person";
+
+            superPerson.powers.AddRange(new[] {"Teleportation",
+                                               "Shockwave",
+                                               "Telekinesis"});
+            superPerson.mask = "Neon Eyes";
+            superPerson.costume = "White Cloak";
+
+            superPerson.moralCode.AddRange(new[] {"Avoid harm if possible",
+                                                  "Taking the life of your enemies makes you as bad as the enemy",
+                                                  "Respect everyone"});
+
+            superPerson.enemies.AddRange(new[] { "Shadow Sovereign",
+                                                 "Vortex Marauder",
+                                                 "Chaos Weaver" });
+
+            superPerson.AddPersonalIssues(new[] {"Parents passed away when younger",
+                                                 "Uncle recently passed away",
+                                                 "Love of their life targeted by a villain"});
+
+            Console.WriteLine($"Hero information:\n\n{superPerson.GetHeroInformation()}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SuperHero.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Superheroes
+{
+    /// <summary>
+    /// Class SuperHero contains string attributes and lists to define the superhero.
+    /// It contains a method that returns information about the hero, and
+    /// methods to set hidden attributes.
+    /// </summary>
+    internal class SuperHero
+    {
+        #region Attributes
+        private string hiddenIdentity = "";
+        public string alias = "";
+        public List<string> powers = new List<string>();
+        public string mask = "";
+        public string costume = "";
+        public List<string> moralCode = new List<string>();
+        public List<string> enemies = new List<string>();
+        private List<string> personalIssues = new List<string>();
+        #endregion
+
+        // Returns information about the hero
+        public string GetHeroInformation()
+        {
+            string info =
+                $"Hidden identity: {hiddenIdentity} \n" +
+                $"Alias: {alias} \n" +
+                $"Powers: {string.Join(", ", powers)} \n" +
+                $"Mask: {mask}  \n" +
+                $"Costume: {costume}  \n" +
+                $"Moral code: {string.Join(", ", moralCode)}  \n" +
+                $"Enemies: {string.Join(", ", enemies)}  \n" +
+                $"Personal issues: {string.Join(", ", personalIssues)}  \n";
+            return info;
+        }
+
+        #region Private Attribute Methods
+        public void SetIdentity(string hiddenIdentity)
+        {
+            this.hiddenIdentity = hiddenIdentity;
+        }
+
+        public void AddPersonalIssues(string[] personalIssues)
+        {
+            this.personalIssues.AddRange(personalIssues);
+        }
+        #endregion
+    }
+}
